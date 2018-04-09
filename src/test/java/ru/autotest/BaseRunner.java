ae9aaf828be9612b15f7485d33bf1b63f8d4f73b
Anzak24328 <doroninv94@gmail.com>
2018-04-09 08:16:43 +0400

    initial commit

---
+++ src/test/java/ru/autotest/BaseRunner.java
@@ -0,0 +1,55 @@
+package ru.autotest;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class BaseRunner {
+    public static ThreadLocal<WebDriver> tl = new ThreadLocal<>();
+    public WebDriver driver;
+    public String browserName = System.getProperty("browser");
+    public String baseUrl;
+
+
+    @BeforeTest
+    public void setUp(){
+        if (tl.get() != null)  driver = tl.get();
+        else {
+            driver = getDriver();
+            tl.set(driver);
+        }
+        driver.manage().window().maximize();
+        baseUrl = "https://www.tinkoff.ru/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            driver.quit();
+            driver = null;
+        }));
+    }
+
+
+    @AfterTest
+    public void tearDown() {
+//        driver.quit();
+    }
+
+    private WebDriver getDriver() {
+        try {
+            BrowserFactory.valueOf(System.getProperty("browser"));
+        } catch (NullPointerException | IllegalArgumentException e) {
+            browserName = randomBrowserPicker();
+            System.setProperty("browser", browserName);
+        }
+        return BrowserFactory.valueOf(browserName).create();
+    }
+
+    private String randomBrowserPicker() {
+        System.out.println("\nThe driver is not set. Running a random browser...");
+        int pick = new Random().nextInt(BrowserFactory.values().length);
+        return BrowserFactory.values()[pick].toString();
+    }
+}
---
+++ src/test/java/ru/autotest/TMobileWebTest.java
@@ -0,0 +1,87 @@
+package ru.autotest;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.fail;
+
+public class TMobileWebTest {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @BeforeClass(alwaysRun = true)
+    public void setUp() {
+        driver = new ChromeDriver();
+        baseUrl = "https://www.tinkoff.ru/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testTestCase() throws InterruptedException {
+        driver.get("https://www.tinkoff.ru/");
+        driver.findElement(By.xpath("//div[@id='x48761']/div[3]/div/div/div/div/div/div/div/div/div/div[6]/a/span")).click();
+        driver.findElement(By.xpath("//input[@id='']")).click();
+        driver.findElement(By.xpath("//input[@id='']")).clear();
+        driver.findElement(By.xpath("//input[@id='']")).sendKeys("ти");
+        driver.findElement(By.xpath("//div[@id='search-and-pay-container']/div[2]/div[2]/div/form/div[3]/div/div/div/div[2]/div/div/div/div/div/div/div/div")).click();
+        for (int second = 0;; second++) {
+            if (second >= 60) fail("timeout");
+            try { if ("Оплатить мобильную связь".equals(driver.getTitle())) break; } catch (Exception e) {}
+            Thread.sleep(1000);
+        }
+
+        driver.findElement(By.xpath("//input[@id='']")).click();
+        driver.findElement(By.xpath("//input[@id='']")).clear();
+        driver.findElement(By.xpath("//input[@id='']")).sendKeys("5");
+        driver.findElement(By.xpath("//button")).click();
+        assertEquals(driver.findElement(By.xpath("//div[2]/div/div/div/div/form/div/div/div[2]")).getText(), "Поле обязательное");
+        assertEquals(driver.findElement(By.xpath("//form/div[2]/div/div/div/div/div/div/div/div[2]")).getText(), "Минимум — 10 ₽");
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDown()  {
+        driver.quit();
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
